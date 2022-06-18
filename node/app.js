af0a8ff47ee104d53470e5ff40abc034a305638e
Matheus Rodrigues <luz.matheus11@gmail.com>
2022-06-18 18:22:24 -0300

    feat: create file url

--- node/app.js
+++ node/app.js
@@ -1,19 +1,37 @@
 const http = require('http');
 const url = require('url');
 const queryString = require('query-string');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
 
-  console.log(req.url);
-  const params = queryString.parse(url.parse(req.url, true).search)
-  console.log(params);
+  let resposta;
+
+  const urlparse = url.parse(req.url, true);
+  const params = queryString.parse(urlparse.search);
+
+  if(urlparse.pathname === '/criar-usuario') {
+  
+  // Criar um usuario
+
+    fs.writeFile(`users/${params.id}.txt`, JSON.stringify(params), function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+
+      res.statusCode = 201;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(resposta);
+    });
+
+    resposta =  'Usuário criado com sucesso';
+  } 
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(params.pergunta);
+  res.end(resposta);
 });
 
 server.listen(port, hostname, () => {
