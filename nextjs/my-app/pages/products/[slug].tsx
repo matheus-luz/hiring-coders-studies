7e468d564f19a34187bc2a91626ca2d62d19c409
Matheus Rodrigues <luz.matheus11@gmail.com>
2022-07-24 10:13:44 -0300

    feat: implement router categories

---
+++ nextjs/my-app/pages/products/[slug].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useRouter } from 'next/router'; 
+
+export default function product() {
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>{router.query.slug}</h1>
+    </>
+  );
+}
